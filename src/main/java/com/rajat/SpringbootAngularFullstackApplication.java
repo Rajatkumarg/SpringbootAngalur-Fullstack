d187431f4c5506631797e3f1ddf3b9da4e19ba1e
Rajatkumarg <rk515651@gmail.com>
2024-10-16 17:37:08 +0530

    Springboot Angular Fullstack

---
+++ src/main/java/com/rajat/SpringbootAngularFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.rajat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularFullstackApplication.class, args);
+	}
+
+}
